dec975479eba809faf8a8bd9d98ed4e867090dff
Kolby Sisk <kolby.sisk@gmail.com>
2024-01-03 12:24:01 -0600

    fix typing

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,23 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export type Next = () => Promise<NextResponse | void>;
-export type Handler<AdditionalReqProperties = any> = (
+
+export type Handler<AdditionalReqProperties = void> = (
   req: NextRequest & AdditionalReqProperties
 ) => NextResponse | Promise<NextResponse>;
-export type HandlerWithEvent<AdditionalReqProperties = any> = (
+
+export type HandlerWithEvent<AdditionalReqProperties = void> = (
   req: NextRequest & AdditionalReqProperties,
   event: NextFetchEvent
 ) => NextResponse | Promise<NextResponse>;
-export type PipeFunction<AdditionalReqProperties = any> = (
+
+export type PipeFunction<AdditionalReqProperties = void> = (
   req: NextRequest & AdditionalReqProperties,
   event: NextFetchEvent,
   next: Next
 ) => Promise<NextResponse | void>;
-export type PipeFunctionOrHandler<AdditionalReqProperties = any> =
+
+export type PipeFunctionOrHandler<AdditionalReqProperties> =
   | Handler<AdditionalReqProperties>
   | HandlerWithEvent<AdditionalReqProperties>
   | PipeFunction<AdditionalReqProperties>;
